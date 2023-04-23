6ed722f3c5d64b7a3c9963042030d3a60257f253
Larry Kagan <larry@superiocity.com>
2023-04-23 17:46:58 -0400

    Use array

--- main.go
+++ main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets int = conferenceTickets
+	var remainingTickets uint = conferenceTickets
+	var bookings [50]string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
@@ -15,7 +16,7 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -27,6 +28,12 @@ func main() {
 	fmt.Scan(&userTickets)
 
 	remainingTickets -= userTickets
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v:\n", bookings)
+	fmt.Printf("The first value: %v:\n", bookings[0])
+	fmt.Printf("Array type: %T:\n", bookings)
+	fmt.Printf("Array length: %v:\n", len(bookings))
 
 	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
 	fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
