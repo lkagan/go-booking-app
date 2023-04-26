0bd0b9bba477ed446486485703be4bf9aef3be8d
Larry Kagan <larry@superiocity.com>
2023-04-26 17:21:29 -0400

    Loop over questions, taking infinite orders

--- main.go
+++ main.go
@@ -13,24 +13,28 @@ func main() {
 	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
-	fmt.Print("Enter your last name: ")
-	fmt.Scan(&lastName)
-	fmt.Print("Enter your email address: ")
-	fmt.Scan(&email)
-	fmt.Print("How many tickets would you like?: ")
-	fmt.Scan(&userTickets)
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Print("Enter your last name: ")
+		fmt.Scan(&lastName)
+		fmt.Print("Enter your email address: ")
+		fmt.Scan(&email)
+		fmt.Print("How many tickets would you like?: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets -= userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("These are all our bookings: %v\n", bookings)
-	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("There are %v tickets remaining.\n", remainingTickets)
+
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 }
